836709eacb5219359d5a8fd6caa53a4707915df1
yakhyaD <yakhyadiom@gmail.com>
2020-12-17 23:06:38 +0000

    fix error on heroku

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -42,7 +42,7 @@
                 </div> <!-- end hero-copy -->
 
                 <div class="hero-image">
-                    <img src="{{ asset('img/macbook-pro-laravel.png') }}" alt="hero image">
+                    <img src="img/macbook-pro-laravel.png" alt="hero image">
                 </div> <!-- end hero-image -->
             </div> <!-- end hero -->
         </header>
